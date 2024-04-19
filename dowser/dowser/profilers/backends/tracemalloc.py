3c0a67da8181a1188fe904f5807d759b7fab0224
delucca <gpg@delucca.dev>
2024-04-19 15:27:30 -0300

    wip: adds tracemalloc

---
+++ dowser/dowser/profilers/backends/tracemalloc.py
@@ -0,0 +1,48 @@
+import time
+import tracemalloc
+
+from typing import Callable
+from threading import Event
+from queue import Queue
+from toolz import compose, first
+from ...core.threading import threaded_wrapper
+from ...core.logging import get_logger
+from ...core.config import config
+from ..types import MemoryUsageWrapper
+
+
+def profile_memory_usage(event: Event, queue: Queue, precision) -> None:
+    logger = get_logger()
+    logger.info(
+        f"Profiling memory usage with tracemalloc backend with precision of {precision}s"
+    )
+
+    tracemalloc.start()
+
+    while not event.is_set():
+        current_memory_usage = get_memory_usage()
+
+        queue.put((current_memory_usage, "b"))
+        time.sleep(precision)
+
+    tracemalloc.stop()
+
+
+get_memory_usage = compose(
+    first,
+    tracemalloc.get_traced_memory,
+)
+
+
+###
+
+
+def wrapper(
+    function: Callable,
+    precision: float,
+    config: dict = config,
+) -> MemoryUsageWrapper:
+    return threaded_wrapper(function, profile_memory_usage, precision)
+
+
+__all__ = ["wrapper"]
